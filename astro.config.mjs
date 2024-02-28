5777dbc3d24cd4c2a75d94c7b902aa65694a50c0
Johannes Knoll <johannes.knoll@tha.de>
2024-02-28 17:14:04 +0000

    fix(config): base url is root

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pyramid-tha-de.github.io',
-  base: '/docs',
+  base: '/',
 	integrations: [
 		starlight({
 			title: 'Pyramid Docs',
